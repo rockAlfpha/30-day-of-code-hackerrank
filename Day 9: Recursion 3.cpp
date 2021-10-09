3665386d10680d7156a63f4d30fe92d33daad2b6
Sourav Shit <90740835+rockAlfpha@users.noreply.github.com>
2021-10-09 08:02:00 +0530

    Add files via upload

---
+++ Day 9: Recursion 3.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int factorial(int n){
+    int r;
+    if(n != 1){
+        r = n * factorial(n-1);
+    }
+    else
+        r = 1;
+    return r;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int r = factorial(n);
+    cout << r << endl;
+    return 0;
+}
